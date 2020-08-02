9edb317c2f1461da2d98b4ba26d93260640d32f5
worthmine <worthmine@gmail.com>
2020-08-02 12:03:49 +0900

    add the tests for tie

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ String::Numeric::Whatever - It's a test implement to
 # SYNOPSIS
 
     use String::Numeric::Whatever;
-    my $str = String::Numeric::Whatever->new( value => 'strings' );
+    my $str = String::Numeric::Whatever->new('strings');
 
     say q|Succeeded in comparing with strings by 'eq'| if $str eq 'strings';            
     say q|Succeeded in comparing with Int by 'ne'|     if $str ne 100;            
@@ -17,7 +17,49 @@ String::Numeric::Whatever - It's a test implement to
 
 # DESCRIPTION
 
-to Do
+## INTRODUCE
+
+If you have knowledge of other language, You may think like that.
+
+_Why strings can't be compared with using `==`?_
+
+I can't answer the reason why, but can give you this module.
+
+It provides us comparable object with using `==`, `eq` or whatever!
+
+## CONSTRUCTORS
+
+I'm sorry that you have to call constructors
+before getting the benefits of this module.
+
+### new()
+
+There is no validation. accepts all types of SCALAR
+
+    my $str = String::Numeric::Whatever->new('strings');
+    my $num = String::Numeric::Whatever->new(1234);
+
+### tie()
+
+or you can set like this:
+
+    tie my $str => 'String::Numeric::Whatever', 'strings';
+    tie my $num => 'String::Numeric::Whatever', 1234;
+
+## THEN
+
+Now you can compare the values with using any operators in below:
+
+    < <= > >= == != <=>
+    lt le gt ge eq ne cmp
+
+After you assigned the constructors,
+you don't have to care about whatever this is a string or number.
+
+So you can write like below without warnings:
+
+say $str if $str == 'string';   # strings 
+say $num if $num ne 0;          # 1234 
 
 # LICENSE
 
--- lib/String/Numeric/Whatever.pm
+++ lib/String/Numeric/Whatever.pm
@@ -11,6 +11,8 @@ our @ISA = qw(Tie::StdScalar);
 use overload (
     '<=>' => \&compareAny,
     'cmp' => \&compareAny,
+    '""'  => sub { $_[0]->FETCH() . '' },
+    '0+'  => sub { $_[0]->FETCH() + 0 },
 );
 
 sub compareAny {
@@ -34,7 +36,7 @@ B<ignore> the difference between C<E<lt>=E<gt>> and C<cmp>
 =head1 SYNOPSIS
 
  use String::Numeric::Whatever;
- my $str = String::Numeric::Whatever->new( value => 'strings' );
+ my $str = String::Numeric::Whatever->new('strings');
 
  say q|Succeeded in comparing with strings by 'eq'| if $str eq 'strings';            
  say q|Succeeded in comparing with Int by 'ne'|     if $str ne 100;            
@@ -43,8 +45,49 @@ B<ignore> the difference between C<E<lt>=E<gt>> and C<cmp>
            
 =head1 DESCRIPTION
 
-to Do
+=head2 INTRODUCE
 
+If you have knowledge of other language, You may think like that.
+
+I<Why strings can't be compared with using C<==>?>
+
+I can't answer the reason why, but can give you this module.
+
+It provides us comparable object with using C<==>, C<eq> or whatever!
+
+=head2 CONSTRUCTORS
+
+I'm sorry that you have to call constructors
+before getting the benefits of this module.
+
+=head3 new()
+
+There is no validation. accepts all types of SCALAR
+
+ my $str = String::Numeric::Whatever->new('strings');
+ my $num = String::Numeric::Whatever->new(1234);
+
+=head3 tie()
+
+or you can set like this:
+
+ tie my $str => 'String::Numeric::Whatever', 'strings';
+ tie my $num => 'String::Numeric::Whatever', 1234;
+
+=head2 THEN
+
+Now you can compare the values with using any operators in below:
+
+ < <= > >= == != <=>
+ lt le gt ge eq ne cmp
+
+After you assigned the constructors,
+you don't have to care about whatever this is a string or number.
+
+So you can write like below without warnings:
+
+say $str if $str == 'string';   # strings 
+say $num if $num ne 0;          # 1234 
 
 =head1 LICENSE
 
